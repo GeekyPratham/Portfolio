b45b9e9fc0b0f0d8cf5de88ac1753de9e9e76ed4
GeekyPratham <kprathamraj2021@gmail.com>
2025-04-29 20:15:20 +0530

    update in middleware file export method

--- backend/middlewares.js
+++ backend/middlewares.js
@@ -1,41 +1,36 @@
-
 const dotenv = require('dotenv');
 dotenv.config();
 
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = process.env.JWT_SECRET;
 
-export const authMiddleware = (req,res,next) =>{
+const authMiddleware = (req, res, next) => {
     const authHeader = req.headers.authorization;
-    // console.log(authHeader)
-    if(!authHeader || !authHeader.startsWith('Bearer ')){
-        return res.status(403).json({
-            Message:"invalid user"
-        })
-    }
-    const token = authHeader.split(' ')[1];
-    // console.log("got the token successfully")
-    // console.log(token)
 
-    // verify the token 
-    try{
-        
-        // console.log("to verify")
-        const decode = jwt.verify(token,JWT_SECRET)
-        // console.log(decode)
-        if(decode.userId){
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+        return res.status(403).json({
+            Message: "invalid user"
+        });
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+        const decode = jwt.verify(token, JWT_SECRET);
+        if (decode.userId) {
             req.userId = decode.userId;
             next();
-        }
-        else{
+        } else {
             res.status(403).json({
-                Message:"invalid user"
-            })
+                Message: "invalid user"
+            });
         }
-    }
-    catch(err){
+    } catch (err) {
         res.status(403).json({
-            Message:"invalid user"
-        })
+            Message: "invalid user"
+        });
     }
-}
+};
+
+
+module.exports = { authMiddleware };
