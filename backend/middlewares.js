25bf93e16ab12c1e73e92f0cb25f1c8e1dc6165f
GeekyPratham <kprathamraj2021@gmail.com>
2025-04-29 20:17:27 +0530

    update in auth middleware

--- backend/middlewares.js
+++ backend/middlewares.js
@@ -6,17 +6,22 @@ const JWT_SECRET = process.env.JWT_SECRET;
 
 const authMiddleware = (req, res, next) => {
     const authHeader = req.headers.authorization;
-
+    // console.log(authHeader)
     if (!authHeader || !authHeader.startsWith('Bearer ')) {
         return res.status(403).json({
             Message: "invalid user"
         });
     }
+    // console.log("got the token successfully")
+    // console.log(token)
 
+    // verify the token
     const token = authHeader.split(' ')[1];
 
     try {
+        // console.log("to verify")
         const decode = jwt.verify(token, JWT_SECRET);
+        // console.log(decode)
         if (decode.userId) {
             req.userId = decode.userId;
             next();
